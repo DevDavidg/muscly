d651e367618839f7250239020261d6ed86fc34e0
David Guillen <dev.davidg@gmail.com>
2025-11-26 18:37:22 -0300

    Add favicon.svg and update layout metadata to include favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Muscly",
   description: "Local music player",
+  icons: {
+    icon: "/favicon.svg",
+  },
 };
 
 export default function RootLayout({
